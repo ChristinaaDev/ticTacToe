945b2abbb394d91df82d2f9c8f2d04dd5b7d932d
Brian Fuentes <Brian.Fuentes04@gmail.com>
2022-05-13 00:11:00 -0700

    added all possible wins

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
+/* All possible combinations to win tictactoe in the form of an array
 
+| 0 | 1 | 2 |
+| 3 | 4 | 5 |
+| 6 | 7 | 8 |
 
+const allPossibleWins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
+All elements of allPossibleWins AND their reverse are all possible wins. 
 
-
+*/
 function placeXOonTile(){
   
 }
